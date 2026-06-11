19088aef07717f9e409aa1acf14c20660649995f
CuevasErik-dev <cuevas.erik.israel@gmail.com>
2026-06-10 19:52:25 -0600

    Configuración inicial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+
+function App() {
+  return (
+    <>
+      <h1 className= "text-3xl font-bold">Soy Erik Israel Cuevas Hernández</h1>
+    </>
+  );
+}
+
+export default App;
